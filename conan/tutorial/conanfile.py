d79726c188ea6effbd04c7528c3cba5f2af5147e
boryssmejda <borys.smejda@gmail.com>
2020-09-27 14:41:39 +0200

    Deleted option to build lib as shared.
    Signed-off-by: boryssmejda <borys.smejda@gmail.com>

--- conan/tutorial/conanfile.py
+++ conan/tutorial/conanfile.py
@@ -20,33 +20,7 @@ class mathOpConan(ConanFile):
         if self.settings.compiler == "Visual Studio":
             del self.settings.compiler.runtime
 
-    def build_windows(self):
-        self.configure()
-        if self.options.shared == True:
-            self.run("cmake -S ./CMake -B . -DBUILD_SHARED_LIBS=ON")
-        else:
-            self.run("cmake -S ./CMake -B .")
-
-        self.run("cmake --build . --config Release --target install")
-        self.run("cmake --build . --config Debug --target install")
-
-
-    def build_linux(self):
-        if self.options.shared == True:
-            self.run("cmake -S ./CMake -B . -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON")
-        else:
-            self.run("cmake -S ./CMake -B . -DCMAKE_BUILD_TYPE=Debug")
-
-        self.run("cmake --build . --target install")
-
-        if self.options.shared == True:
-            self.run("cmake -S ./CMake -B . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON")
-        else:
-            self.run("cmake -S ./CMake -B . -DCMAKE_BUILD_TYPE=Release")
-
-        self.run("cmake --build . --target install")
-
-    def build_android(self):
+    def build(self):
         cmake_release = CMake(self, build_type="Release")
         cmake_release.configure(source_folder="./CMake")
         cmake_release.build()
@@ -56,16 +30,6 @@ class mathOpConan(ConanFile):
         cmake_debug.configure(source_folder="./CMake")
         cmake_debug.build()
         cmake_debug.install()
-
-    def build(self):
-        if self.settings.os == 'Windows':
-            self.build_windows()
-
-        elif self.settings.os == 'Linux':
-            self.build_linux()
-
-        elif self.settings.os == 'Android':
-            self.build_android()
 
     def package(self):
         self.copy("*", dst=".", src="./CMake/install/mathOp")
--- src/mathOp.h
+++ src/mathOp.h
@@ -1,17 +1,8 @@
 #pragma once
 
-#if defined(_WIN32)
-    #ifdef MATHLIBRARY_EXPORTS
-        #define MATHLIBRARY_API __declspec(dllexport)
-    #else
-        #define MATHLIBRARY_API
-    #endif
-#else
-    #define MATHLIBRARY_API
-#endif
-
 namespace mathOp
 {
-    MATHLIBRARY_API int add(int a, int b);
-    MATHLIBRARY_API int subtract(int a, int b);
+    int add(int a, int b);
+    int subtract(int a, int b);
 }
+
