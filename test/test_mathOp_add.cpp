df9960a71e5e55e5ad88b58cf91ddca10d2a1453
boryssmejda <borys.smejda@gmail.com>
2020-10-04 22:31:26 +0200

    Added tests for the library.
    Signed-off-by: boryssmejda <borys.smejda@gmail.com>

--- test/test_mathOp_add.cpp
+++ test/test_mathOp_add.cpp
@@ -1,10 +1,35 @@
+#include "mathOp.h"
+
 #include "gtest/gtest.h"
 
 
 TEST(MathOpAddTest, AddSmallNumbers)
 {
-    EXPECT_EQ(true, true);
+    EXPECT_EQ(mathOp::add(0,0), 0);
+    EXPECT_EQ(mathOp::add(0,1), 1);
+    EXPECT_EQ(mathOp::add(1,1), 2);
+    EXPECT_EQ(mathOp::add(2,1), 3);
+    EXPECT_EQ(mathOp::add(3,1), 4);
 }
+
+TEST(MathOpAddTest, AddBigNumbers)
+{
+    EXPECT_EQ(mathOp::add(1150,850), 2000);
+    EXPECT_EQ(mathOp::add(9999,1), 10000);
+    EXPECT_EQ(mathOp::add(100000,1), 100001);
+    EXPECT_EQ(mathOp::add(100'000'000, 15), 100'000'015);
+}
+
+TEST(MathOpAddTest, DetectOverflow)
+{
+
+}
+
+TEST(MathOpAddTest, DetectUnderflow)
+{
+
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
