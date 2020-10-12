0ad3eccdbf36960bc8610340feb72ec77359c32c
boryssmejda <borys.smejda@gmail.com>
2020-10-12 22:32:10 +0200

    Modified build scripts for MacOS.
    Signed-off-by: boryssmejda <borys.smejda@gmail.com>

--- scripts/macos/configure.sh
+++ scripts/macos/configure.sh
@@ -2,7 +2,8 @@
 
 . ./project_paths.sh
 
-cmake   -S $PROJECT_ROOT \
+cmake   -G Xcode \
+        -S $PROJECT_ROOT \
         -B $BUILD_DIR \
         -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
         -D CMAKE_PREFIX_PATH="$DEPS_DIR" \
