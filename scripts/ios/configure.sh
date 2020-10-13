53d07334bee940cd9d1237e9e657184f30f3d04a
boryssmejda <borys.smejda@gmail.com>
2020-10-13 15:44:46 +0200

    Added iOS scripts.

---
+++ scripts/ios/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ./project_paths.sh
+
+cmake   -G Xcode \
+        -S $PROJECT_ROOT \
+        -B $BUILD_DIR \
+        -D CMAKE_SYSTEM_NAME=iOS \
+        -D CMAKE_OSX_ARCHITECTURES=arm64 \
+        -D CMAKE_OSX_DEPLOYMENT_TARGET=13.0 \
+        -D CMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=YES \
+        -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+        -D CMAKE_PREFIX_PATH="$DEPS_DIR" \
+        -D FMT_HEADERS_LOCATION="$FMT_HEADERS_LOCATION" \
+        -D BUILD_TESTS=ON \
---
+++ scripts/ios/uninstall_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+. ./project_paths.sh
+
+rm -rf $DEPS_DIR
