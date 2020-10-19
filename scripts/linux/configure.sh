3ca98c744fef2b61a3c10b5e4471ebebdb814a79
boryssmejda <borys.smejda@gmail.com>
2020-10-19 14:44:53 +0200

    Solved problem of accidental symbol export of mathOp lib in C API.
    Signed-off-by: boryssmejda <borys.smejda@gmail.com>

--- scripts/linux/configure.sh
+++ scripts/linux/configure.sh
@@ -3,22 +3,9 @@
 . ./project_paths.sh
 
 if [ $# -eq 0 ]; then
-    echo >&2 "No build type given!"
-    exit 1
-fi
-
-#if [ "$1" = "Debug" ]; then
-#    buildType="Debug"
-#elif [ "$1" = "Release" ]; then
-#    buildType="Release"
-#else
-#    echo "Unknown Configuration! Aborting ..."
-#    exit 1
-#fi
-
-#echo "Build configuration: $buildType"
-
-if [ "$1" = "Static" ]; then
+    shouldBuildShared="OFF"
+    echo "No configuration given. Default one chosen: Static"
+elif [ "$1" = "Static" ]; then
     shouldBuildShared="OFF"
 elif [ "$1" = "Shared" ]; then
     shouldBuildShared="ON"
