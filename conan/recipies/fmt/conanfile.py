83c3c7231485e55f6a23fd86170a5a648de5771d
boryssmejda <borys.smejda@gmail.com>
2020-10-23 17:34:41 +0200

    Added fmt binary package using Conan.

--- conan/recipies/fmt/conanfile.py
+++ conan/recipies/fmt/conanfile.py
@@ -1,6 +1,6 @@
 import os
 
-from conans import ConanFile, tools
+from conans import ConanFile, tools, CMake
 
 
 class FmtConan(ConanFile):
@@ -11,15 +11,33 @@ class FmtConan(ConanFile):
     url = "https://github.com/fmtlib/fmt.git"
     description = "{fmt} is an open-source formatting library for C++."
     topics = ("c++", "formatting", "performance")
-    no_copy_source = True
-    # No settings/options are necessary, this is header only
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "header_only": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "header_only": False, "fPIC": True}
 
     def source(self):
         self.run("git clone {0}".format(self.url))
         self.run("cd fmt && git checkout tags/{0} -b v{0}-branch".format(self.version))
 
+    def build(self):
+        cmake_release = CMake(self, build_type="Release")
+        cmake_release.definitions["FMT_DOC"] = "OFF"
+        cmake_release.definitions["FMT_TEST"] = "OFF"
+        cmake_release.configure(source_folder="./fmt")
+        cmake_release.build()
+        cmake_release.install()
+
+        cmake_debug = CMake(self, build_type="Debug")
+        cmake_debug.definitions["FMT_DOC"] = "OFF"
+        cmake_debug.definitions["FMT_TEST"] = "OFF"
+        cmake_debug.configure(source_folder="./fmt")
+        cmake_debug.build()
+        cmake_debug.install()
+
     def package(self):
-        self.copy("*h", dst="include/fmt", src="fmt/include/fmt")
+        #self.copy("*h", dst="include/fmt", src="fmt/include/fmt")
+        pass
 
     def package_id(self):
-        self.info.header_only()
+        pass
+        #self.cpp_info.libs = ["fmt"]
